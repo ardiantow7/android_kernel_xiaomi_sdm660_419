a2395ba5203da3d1d1b06e6538141b403bc79ab6
kucingoranye <ardiantow04@gmail.com>
2026-09-22 09:12:51 +0700

    static symbol export

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -455,7 +455,7 @@ static int dump_masked_av_helper(void *k, void *d, void *args)
 	return 0;
 }
 
-static void security_dump_masked_av(struct policydb *policydb,
+void security_dump_masked_av(struct policydb *policydb,
 				    struct context *scontext,
 				    struct context *tcontext,
 				    u16 tclass,
@@ -620,7 +620,7 @@ void services_compute_xperms_drivers(
  * Compute access vectors and extended permissions based on a context
  * structure pair for the permissions in a particular class.
  */
-static void context_struct_compute_av(struct policydb *policydb,
+void context_struct_compute_av(struct policydb *policydb,
 				      struct context *scontext,
 				      struct context *tcontext,
 				      u16 tclass,
